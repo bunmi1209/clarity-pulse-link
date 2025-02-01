0f68893c552c582f5443b8e38d510acd3ed63f59
Bunmi1209 <owolabioluwabunmihellenhellen@gmail.com>
2025-02-01 02:03:15 +0100

    test: Add health data contract tests

---
+++ tests/health-data_test.ts
@@ -0,0 +1,26 @@
+import {
+  Clarinet,
+  Tx,
+  Chain,
+  Account,
+  types
+} from 'https://deno.land/x/clarinet@v1.0.0/index.ts';
+import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
+
+Clarinet.test({
+  name: "Ensure user can store health data",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const wallet_1 = accounts.get("wallet_1")!;
+    
+    let block = chain.mineBlock([
+      Tx.contractCall("health-data", "store-health-data", 
+        [types.uint(75), types.uint(10000), types.utf8("device-123")], 
+        wallet_1.address
+      )
+    ]);
+    
+    assertEquals(block.receipts.length, 1);
+    assertEquals(block.height, 2);
+    assertEquals(block.receipts[0].result.expectOk(), true);
+  },
+});
